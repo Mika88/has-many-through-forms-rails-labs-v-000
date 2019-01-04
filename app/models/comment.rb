dcf683e31fb336346e4e5a2d62fa2275ed3bd42c
Mika88 <mikaspivak@gmail.com>
2019-01-04 01:17:34 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,6 @@ class Comment < ActiveRecord::Base
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
       user = User.find_or_create_by(user_attribute)
-      self.user = user
     end
   end
 end
