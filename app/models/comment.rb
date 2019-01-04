f460ffb5c55d2d13731b4b5ee7d39373141c67cc
Mika88 <mikaspivak@gmail.com>
2019-01-04 01:16:06 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: :user == nil
 
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.user = user
 end
